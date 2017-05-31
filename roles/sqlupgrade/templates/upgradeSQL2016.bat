ca37a0b0ad25d77fdc387aed6b8b9dec9a3fa3ef
oatakan <oatakan@gmail.com>
2017-05-31 01:31:12 -0500

    initial commit

---
+++ roles/sqlupgrade/templates/upgradeSQL2016.bat
@@ -0,0 +1,1 @@
+\\172.16.32.239\Installs\SQL_2016_Ent\setup.exe /Q /ACTION=upgrade /IACCEPTSQLSERVERLICENSETERMS /ENU /UPDATEENABLED=false /ConfigurationFile=\\172.16.32.239\Installs\SQL_2016_Ent\Upgrade.ini
