212593ab8b098685cbd6e46102ae978cdabd283c
oatakan <oatakan@gmail.com>
2017-10-04 15:30:28 -0500

    add sql server management studio install

--- roles/sql2016/files/enableSQLTCP.ps1
+++ roles/sql2016/files/enableSQLTCP.ps1
@@ -1,3 +1,9 @@
+# Ensure SQL Power Shell Modules are in the path
+$env:PSModulePath = $env:PSModulePath + ";C:\Program Files (x86)\Microsoft SQL Server\130\Tools\PowerShell\Modules"
+
+# Import SQL Server Module called SQLPS
+Import-Module SQLPS -DisableNameChecking
+
 $smo = 'Microsoft.SqlServer.Management.Smo.'
 $wmi = new-object ($smo + 'Wmi.ManagedComputer')
 # Enable the TCP protocol on the default instance.  If the instance is named, replace MSSQLSERVER with the instance name in the following line.
